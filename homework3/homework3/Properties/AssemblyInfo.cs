dfe4a43eacd8a64ff359406507bf2a3d19163116
qxpineapple <1312661951@qq.com>
2018-09-26 17:42:11 +0800

    homewrok3.1

--- homework3/homework3/Properties/AssemblyInfo.cs
+++ homework3/homework3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("homework3")]
+[assembly: AssemblyTitle("ConsoleApp1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("homework3")]
+[assembly: AssemblyProduct("ConsoleApp1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("4357e86e-5f9c-4fcb-9875-841f68a3ebaf")]
+[assembly: Guid("eef04e61-5edb-406a-bd37-0b005e088db5")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
