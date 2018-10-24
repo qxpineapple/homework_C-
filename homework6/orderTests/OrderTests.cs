2208b3184c507216c266a5838432e4b2cb5a2819
qxpineapple <1312661951@qq.com>
2018-10-24 12:19:59 +0800

    homework6

---
+++ homework6/orderTests/OrderTests.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace order.Tests
+{
+    [TestClass()]
+    public class OrderTests
+    {
+        Order order = new Order("Customer", "Item", 3);
+        //删除测试
+        [TestMethod()]
+        public void OrderDelet0()
+        {
+            OrderService obj = new OrderService();
+            int n = OrderService.orderList.Count();
+            obj.OrderDelete(0);
+            Assert.IsTrue(n - 1 == OrderService.orderList.Count());
+        }
+        [TestMethod()]
+        public void OrderDelet1()
+        {
+            OrderService obj = new OrderService();
+            int n = OrderService.orderList.Count();
+            //错误数据
+            obj.OrderDelete(1);
+            Assert.IsTrue(n - 1 == OrderService.orderList.Count());
+        }
+        //改变数据测试
+        [TestMethod()]
+        public void ChangeTest0()
+        {
+            OrderService obj = new OrderService();
+        }
+        [TestMethod()]
+        public void ChangeTest1()
+        {
+            OrderService obj = new OrderService();
+        }
+        //增加测试
+        [TestMethod()]
+        public void InsertOrderTest0()
+        {
+            OrderService obj = new OrderService();
+            OrderDetails newobj = new OrderDetails("NewCustomer", "NewItem", 4);
+            int n = OrderService.orderList.Count();
+            obj.InsertOrder(newobj);
+            Assert.IsTrue(n == OrderService.orderList.Count() - 1);
+        }
+        [TestMethod()]
+        public void InsertOrderTest1()
+        {
+            OrderService obj = new OrderService();
+            OrderDetails newobj = new OrderDetails();
+            int n = OrderService.orderList.Count();
+            obj.InsertOrder(newobj);
+            Assert.IsTrue(n == OrderService.orderList.Count() - 1);
+        }
+        //查找测试
+        [TestMethod()]
+        public void SeekTest()
+        {
+            OrderService obj = new OrderService();
+            int n0 = obj[1];
+            int n1 = obj["Customer"];
+            int n2 = obj["Item"];
+            Assert.IsFalse(n0 == -1 && n1 == -1 && n2 == -1);
+        }
+        [TestMethod()]
+        public void FalseSeekTest()
+        {
+            OrderService obj = new OrderService();
+            //错误数据
+            int n0 = obj[2];
+            int n1 = obj["NewCustomer"];
+            int n2 = obj["NewItem"];
+            Assert.IsTrue(n0 == -1 && n1 == -1 && n2 == -1);
+        }
+        //xml序列化测试
+        [TestMethod()]
+        public void XmlTest0()
+        {
+            string FileName = "xmlTest.xml";
+            OrderService.Export(FileName);
+            Assert.IsTrue(File.Exists(FileName));
+            
+        }
+        [TestMethod()]
+        public void XmlTest1()
+        {
+            //错误数据
+            string FileName = "";
+            OrderService.Export(FileName);
+            Assert.IsTrue(File.Exists(FileName));
+        }
+    }
+}
